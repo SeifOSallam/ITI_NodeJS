e8f0c5d83aad54515c6e308f5f83963fc41ec06f
SeifOSallam <seif.osallam@gmail.com>
2024-02-06 00:40:53 +0200

    Lab 2

---
+++ Lab_2/index.js
@@ -0,0 +1,50 @@
+const fs = require('fs')
+const http = require('http')
+const todos = require('./todos.js')
+http.createServer((req, res) => {
+  let filedata = ""
+  let stylesdata = ""
+  if (req.url == "/") {
+      res.setHeader("content-type", "text/html")
+      const todosFile = fs.createReadStream("./TodoList.json", 'utf-8');
+      const homeStyles = fs.createReadStream("./styles.css", 'utf-8')
+      todosFile.pause();
+      homeStyles.on('data' , (chunk) => {
+        stylesdata += chunk
+      })
+      homeStyles.on('end' , () => {
+        res.write(`<head><style>${stylesdata}</style></head>`)
+        todosFile.resume()
+      })
+      todosFile.on('data' ,(chunk) => {
+        filedata += chunk;
+      })
+      todosFile.on('end', () => {
+        const jsonData = JSON.parse(filedata);
+        jsonData.forEach((todo) => {
+          res.write(`<p class='${todo.status}'>${todo.title}</p>`)
+        })
+        res.end();
+      })
+    }
+    else if(req.url == "/astronomy") {
+      const astronomyImage = fs.createReadStream('./images/fossils.jpg', 'base64')
+      res.setHeader("content-type", 'text/html')
+      let imgData = ""
+      astronomyImage.on('data', (chunk) => {
+        imgData += chunk;
+      })
+      astronomyImage.on('end', () => {
+        const imgSrc = `data:image/jpeg;base64,${imgData}`
+        res.write(`<img src="${imgSrc}">`)
+        res.end()
+      })
+    }
+    else {
+      res.write('<h1>404 PAGE NOT FOUND</h1>')
+      res.end()
+    }
+
+}).listen(3000, () => {
+  console.log("Server is listening on 9000....")
+})
